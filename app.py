bac11a638b491782f54194132f98c7273cc31e90
Amen-code <83923570+Amen-code@users.noreply.github.com>
2024-03-08 08:07:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 # List of random visitor names
 
 def main():
-    st.set_page_config(" Hammamet conference 2023-2024",page_icon="conference.png")
+    st.set_page_config(" Hammamet conference 2024",page_icon="conference.png")
     st.title("Stochastics in Mathematical Finance and Physics Conference")
     st.markdown('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">', unsafe_allow_html=True)
     st.markdown("Time and place: <i class='fas fa-clock'></i> Oct. 21–Oct. 25, 2023, <i class='fas fa-map-marker-alt'></i> Radisson Blue Resort Thalasso, Hammamet, Tunisia", unsafe_allow_html=True)
@@ -42,7 +42,7 @@ def main():
     st.write("The best way to reach the conference venue for international participants is to fly to the International airport of Tunis-Carthage. Local transportation between the airport and the conference hotel is organized and included in the rates. Please fill the registration form above to secure the service.")
 
     st.subheader(":blue[Sponsors]")
-    st.write("This event is organized by the collaboration of several universities and organizations. Financial support is also received from FWO Scientific Research Network ModSimFIE and DAAD- Deutscher Akademischer Austauschdienst with funds from the German Foreign Office.")
+    st.write("This event is organized by the collaboration of several universities and organizations. Financial support is also received from FWO Scientific Research Network ModSimFIE")
 
     st.subheader(":blue[Organizers]")
     organizers = [
@@ -53,7 +53,8 @@ def main():
         "**Asma Khedher**", 
         "**Astrid Hilbert**", 
         "**Bernt Øksendal**", 
-        "**Barbara Rüdiger**", 
+        "**Barbara Rüdiger**",
+        "**Josef Teichmann**",
         "**Nizar Touzi**",
         "**Michèle Vanmaele**"
     ]
