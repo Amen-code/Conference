0841a63d9d9ddb9ee48b5705cb5f52fd695f8db3
Amen-code <83923570+Amen-code@users.noreply.github.com>
2024-03-08 09:18:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ def main():
     st.title("Stochastics in Mathematical Finance and Physics Conference")
     st.markdown('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">', unsafe_allow_html=True)
     st.markdown("Time and place: <i class='fas fa-clock'></i> Oct. 21–Oct. 25, 2024, <i class='fas fa-map-marker-alt'></i> Radisson Blue Resort Thalasso, Hammamet, Tunisia", unsafe_allow_html=True)
-    st.divider()
     st.write("The conference is an occasion to bring together researchers in the beautiful Hammamet to discuss recent developments in stochastics with applications to mathematical finance and  physics  .")
     
     # Display conference name
