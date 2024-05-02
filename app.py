942744afbee1c85a6cfde65912b7bb84894183b9
Amen-code <83923570+Amen-code@users.noreply.github.com>
2024-05-02 18:23:10 +0200

    i reordered the section and added the Fees section

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ import streamlit as st
 
 
 def generate_card(name, university):
-    st.write(f"**{name}  {university}**")
+    st.write(f"**{name}**")
+    st.write(f"*{university}*")
     
     
 def main():
-    st.set_page_config(" Hammamet conference 2024",page_icon="Logo.png")
+    st.set_page_config(" Hammamet conference 2023-2024",page_icon="Logo.png")
     st.title("Stochastics in Mathematical Finance and Physics Conference")
     st.markdown('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">', unsafe_allow_html=True)
-    st.markdown("Time and place: <i class='fas fa-clock'></i> Oct. 21–Oct. 25, 2024, <i class='fas fa-map-marker-alt'></i> Radisson Blue Resort Thalasso, Hammamet, Tunisia", unsafe_allow_html=True)
-    st.write("The conference is an occasion to bring together researchers in the beautiful Hammamet to discuss recent developments in stochastics with applications to mathematical   finance and  physics.")
+    st.markdown("Time and place: <i class='fas fa-clock'></i> Oct. 21-Oct. 25, 2023, <i class='fas fa-map-marker-alt'></i> Radisson Blue Resort Thalasso, Hammamet, Tunisia", unsafe_allow_html=True)
+    st.write("---")
+    st.write("The conference is an occasion to bring together researchers in the beautiful Hammamet to discuss recent developments in stochastics with applications to mathematical physics and finance.")
     # Display conference name
     st.markdown("""
     
@@ -30,38 +32,48 @@ def main():
     # Display hotel image
     
     st.image('hotel.jpg', width = 600 ,output_format = "JPEG")
-
-    # Button to register (redirects to Google Form)
-    st.subheader(":blue[ For Registration]")
-    st.write("The registration form is available [here](https://docs.google.com/forms/d/e/1FAIpQLScJtPavmI45WvgrrMSHVJUc6xAcEktuBd--JZ53DgQWjVBZXg/viewform).")
-    st.error("Deadline for submission is September 5th.")
-
+    # Call for Abstract 
+    st.subheader(":blue[Call for Abstracts]")
+    st.write("A call for abstract is open to propose a contributed talk or a poster. There are limited slots for contributed talks. The form is [here](https://forms.gle/FXaQ37w8x7CHxDL99).")
+    st.error("Deadline for submission is September 5th. You will be notified about your proposed talk/poster within September 15th.")
+    
+    #Program
     st.subheader(":blue[Program]")
     st.write("A full list of speakers will be available together with the program.")
+    # Button to register (redirects to Google Form)
+    st.subheader(":blue[Registration]")
+    st.write("The registration form is available [here](https://docs.google.com/forms/d/e/1FAIpQLScJtPavmI45WvgrrMSHVJUc6xAcEktuBd--JZ53DgQWjVBZXg/viewform).")
+    st.error("Deadline for submission is September 5th. You will be notified about your proposed talk/poster within September 15th.")
+    # URL for the International airport of Tunis-Carthage
+    airport_url = "https://www.tunis-airport.com"
 
-    
-    st.subheader(":blue[ Call For Abstract]")
-    st.write(" A call for abstract is open to propose a contributed talk or a poster. The form is   [here]( https://docs.google.com/forms/d/e/1FAIpQLSfd957l7l_U6Vi1_bLuK79Qf72uC6RvSQ1alwZmSRl9xOhLXQ/viewform?usp=pp_url ) .")
-    
+    # Adding a link to the airport within the Transport section
     st.subheader(":blue[Transport]")
-    st.write("The best way to reach the conference venue for international participants is to fly to the International airport of Tunis-Carthage. Local transportation between the airport and the conference hotel is organized and included in the rates. Please fill the registration form above to secure the service.")
+    st.markdown("The best way to reach the conference venue for international participants is to fly to the [International airport of Tunis-Carthage]({}). Local transportation between the airport and the conference hotel is organized and included in the rates. Please fill the registration form above to secure the service.".format(airport_url))
+
+    hotel_name = "Radisson Blue Resort Thalasso, Hammamet"
+    hotel_url = "https://www.radissonhotels.com/en-us/hotels/radisson-blu-resort-hammamet"
+
+    st.subheader(":blue[Fees]")
+    st.markdown(f"The conference is organised at the **[{hotel_name}]({hotel_url})**. The hotel provides all the necessary services for the event in a beautiful location. The local organisers have arranged some special rates for the days of the event for the registered participants: Full-board basis (night, breakfast, lunch, coffee breaks, dinner and transportation from Tunis airport to the hotel ): €140 per person and per day. Accompanying persons are accommodated with full-board at €100 per person and per day.")
 
     st.subheader(":blue[Sponsors]")
-    st.write("This event is organized by the collaboration of several universities and organizations. Financial support is also received from FWO Scientific Research Network ModSimFIE .")
+    st.write("This event is organized by the collaboration of several universities and organizations. Financial support is also received from FWO Scientific Research Network ModSimFIE and DAAD- Deutscher Akademischer Austauschdienst with funds from the German Foreign Office.")
 
     st.subheader(":blue[Organizers]")
     organizers = [
-    ("Saloua Mani Aouadi", "(Tunis El Manar University)"),
-    ("Giulia di Nunno", "(University of Oslo)"),
-    ("Olfa Draouil", "(Tunis El Manar University)"),
-    ("Martin Friesen", "(Dublin City University)"),
-    ("Asma Khedher", "(University of Amsterdam)"),
-    ("Astrid Hilbert", "(Linnaeus University)"),
-    ("Bernt Øksendal", "(University of Oslo)"),
-    ("Barbara Rüdiger", "(Bergische University Wuppertal)"),
-    ("Nizar Touzi", "(New York University)"),
-    ("Michèle Vanmaele", "(Ghent University)"),
-    ("Josef Teichmann", "(ETH Zürich)")
+    ("Saloua Mani Aouadi", "Tunis El Manar University"),
+    ("Giulia di Nunno", "University of Oslo"),
+    ("Olfa Draouil", "Tunis El Manar University"),
+    ("Martin Friesen", "Dublin City University"),
+    ("Asma Khedher", "University of Amsterdam"),
+    ("Astrid Hilbert", "Linnaeus University"),
+    ("Bernt Øksendal", "University of Oslo"),
+    ("Barbara Rüdiger", "Bergische University Wuppertal"),
+    ("Josef Teichmann", "ETH Zürich"),
+    ("Nizar Touzi", "New York University"),
+    ("Michèle Vanmaele", "Ghent University"),
+    
 ]
     for i in range(0, len(organizers), 3):
         row = organizers[i:i+3]
@@ -76,9 +88,7 @@ def main():
         with col3:
             if i + 2 < len(organizers):
                 generate_card(row[2][0], row[2][1])
-    
-    st.image("Logo.png")
-    
+    st.write("---")
     
 if __name__ == '__main__':
     main()
